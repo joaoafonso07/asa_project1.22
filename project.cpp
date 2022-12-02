22a1c1c5cb6e4645fe8ea4f6a45cfb1319a371dd
afonso <joaoafonsomestre@gmail.com>
2022-12-02 09:09:58 +0000

    Reading input implemented in project.cpp

--- project.cpp
+++ project.cpp
@@ -1,1 +1,15 @@
-//test
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, m; //n = number of lines, m = number of columns 
+    scanf("%d\n%d\n", n, m);
+    
+    int c[m]; //array with m ci's
+
+    for(int i = 0; i < m; i++){ // for each i less than m add the ci to the array
+        scanf("%d\n", c[i]);
+    }
+    
+    return 0;
+}
