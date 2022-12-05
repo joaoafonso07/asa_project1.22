7a6b1a7f1792167376e542778275409e28b69eb6
joaoafonso07 <joaoafonsomestre@gmail.com>
2022-12-05 07:50:32 +0000

    Bugs fixed
    Some bugs were fixed during meeting

--- project.cpp
+++ project.cpp
@@ -5,11 +5,14 @@ int main() {
     int n, m; //n = number of lines, m = number of columns 
     scanf("%d\n%d\n", n, m);
     
-    int c[m]; //array with m ci's
+    int c[n]; //array with n ci's
+    int small;
 
-    for(int i = 0; i < m; i++){ // for each i less than m add the ci to the array
+    for(int i = 0; i < n; i++){ // for each i less than n add the ci to the array
         scanf("%d\n", c[i]);
+        
     }
     
+
     return 0;
 }
