e773e236c16ef78680ecf9bcb77364414e04c36d
joaoafonso07 <joaoafonsomestre@gmail.com>
2022-12-11 14:11:48 +0000

    Input reading | explorer point | max_tile function
    Input reading is working without bugs.
    Explorer point seems to be alright
    And max_tile functions is working.
    
    Remaisn to find oput how to implement recursion.

--- project.cpp
+++ project.cpp
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef struct{
+    int x; //horizontal
+    int y; //vertical
+}point;
+
+int max_tile(point p,int n);
+
+int max_tile(point p,int n){
+    return min(p.x, n - p.y);
+}
+
+
+
+
+
 
 int main() {
     int n, m; //n = number of lines, m = number of columns 
-    scanf("%d\n%d\n", n, m);
-    
+    scanf("%i", &n);
+    scanf("%i", &m);
     int c[n]; //array with n ci's
     int small;
-
-    for(int i = 0; i < n; i++){ // for each i less than n add the ci to the array
-        scanf("%d\n", c[i]);
-        
-    }
+    point exp_pt;
     
+   for(int i = 0; i < n; i++){ // for each i less than n add the ci to the array
+        scanf("%d", &c[i]);
+    }
+
+    for(int i = 0; i < n-1; i++){
+        if (c[i] > 1){
+            exp_pt.x = c[i];
+            exp_pt.y = i + 1;
+            break;
+        }
+    }
+
+    int max = max_tile(exp_pt, n);
+
 
     return 0;
 }
