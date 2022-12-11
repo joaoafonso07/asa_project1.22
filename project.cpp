d58e6dd18c4ae8a5e2f3f3ad64aa42f1a0f26726
joaoafonso07 <joaoafonsomestre@gmail.com>
2022-12-11 20:21:14 +0000

    11/12/22
    Need to impelment a better way to find the next explorer point

--- project.cpp
+++ project.cpp
@@ -10,38 +10,47 @@ typedef struct{
 }point;
 
 int max_tile(point p,int n);
+point next_exp_pt(int c[], int n);
 
 int max_tile(point p,int n){
     return min(p.x, n - p.y);
 }
 
+point next_exp_pt(int c[], int n, point previous){
+    point exp_pt;
+    
+    for(int i = 0; i < n-1; i++){
+        if (c[i] > 1){
+            exp_pt.x = c[i];
+            exp_pt.y = i + 1;
+            return exp_pt;
+        }
+    }
+    exp_pt.x = -1;
+    return exp_pt;
+}
 
-
-
-
+int tilling_numb(int c[], int n){
+    point exp_pt = next_exp_pt(c, n);
+    int max = max_tile(exp_pt, n);
+    for (max; max > 0; max--){
+        tilling_numb()
+    }
+}
 
 int main() {
     int n, m; //n = number of lines, m = number of columns 
     scanf("%i", &n);
     scanf("%i", &m);
     int c[n]; //array with n ci's
-    int small;
-    point exp_pt;
+    
     
    for(int i = 0; i < n; i++){ // for each i less than n add the ci to the array
         scanf("%d", &c[i]);
     }
 
-    for(int i = 0; i < n-1; i++){
-        if (c[i] > 1){
-            exp_pt.x = c[i];
-            exp_pt.y = i + 1;
-            break;
-        }
-    }
 
-    int max = max_tile(exp_pt, n);
-
+    tilling_numb(c, n);
 
     return 0;
 }
